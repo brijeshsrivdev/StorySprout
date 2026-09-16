5e5a0572f77ab573e593cbe099b5987a02acfc49
Brijesh Srivastava <85293821+brijeshsrivdev@users.noreply.github.com>
2026-09-16 15:44:11 +0530

    feat: add Story Outline generation error

---
+++ apps/api/src/main/java/com/storysprout/api/outline/OutlineGenerationException.java
@@ -0,0 +1,5 @@
+package com.storysprout.api.outline;
+
+public class OutlineGenerationException extends RuntimeException {
+    public OutlineGenerationException(String message) { super(message); }
+}
